9e48230b6d7d692f26f97dca7df815abf7c0f99f
Zhiguang Liu <zhiguang.liu@intel.com>
2019-07-18 14:29:11 +0800

    delete the change of Baselib

--- MdePkg/Library/BaseLib/ChkStkGcc.c
+++ MdePkg/Library/BaseLib/ChkStkGcc.c
@@ -9,22 +9,10 @@
 #include "BaseLibInternals.h"
 
 /**
-  Hack function for passing GCC build. This is for X64.
+  Hack function for passing GCC build.
 **/
 VOID
 __chkstk()
 {
 }
 
-/*
-  Hack function for passing CLANGWIN build. This is for IA32.
-  https://metricpanda.com/rival-fortress-update-45-dealing-with-__chkstk-__chkstk_ms-when-cross-compiling-for-windows
-
-  LLVM and mingw don't support __chkstk disabling
-  Unfortunately LLVM's code generator for Windows targets use an hard-coded probe size of 4K as of LLVM 5.0,
-  and this size can only be changed on a per-function basis with the stack-probe-size function attribute.
-*/
-VOID
-_chkstk()
-{
-}
